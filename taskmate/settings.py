551a286e9c9ff952e6fc24f282b55275b376ee61
unknown <koppakasatya1996@gmail.com>
2025-02-14 12:42:37 +0530

    Deployment Update

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 #DEBUG = True
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -138,8 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [(os.path.join(BASE_DIR,'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
